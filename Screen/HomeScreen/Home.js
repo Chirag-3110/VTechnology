f842fd7f55569a29d6d683ed090443dcad863d99
abhijan2402 <abhishek.jangid643@gmail.com>
2022-11-14 20:30:26 +0530

    Adding suggestion list

--- Screen/HomeScreen/Home.js
+++ Screen/HomeScreen/Home.js
@@ -1,12 +1,80 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, FlatList, Image, StyleSheet, } from 'react-native'
+import DATA from "../../Data/CourseSuggestion"
 const Home = () => {
+    const renderItem = ({ item }) => (
+        <View style={styles.ListView}>
+            <View>
+
+                <Image
+                    style={styles.ImgLogo}
+                    source={{
+                        uri: item.ImgUrl
+                    }} />
+            </View>
+            <View style={styles.TitleView}>
+
+                <Text style={styles.TitleViewText}>{item.title}</Text>
+            </View>
+        </View>
+    );
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{color:"black"}}>Home!</Text>
+        <View style={{ flex: 1 }}>
+            <View style={styles.OptionView}>
+                <Text style={styles.OptionViewText}>Made for you</Text>
+            </View>
+            <View style={styles.FlatListView}>
+
+                <FlatList
+                    horizontal={true}
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                />
+            </View>
         </View>
     )
 }
-
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+    ImgLogo: {
+        width: 110,
+        height: 110,
+        borderRadius: 11
+    },
+    ListView: {
+        marginHorizontal: 15
+    },
+    FlatListView: {
+        marginHorizontal: 5
+    },
+    TitleView: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 10
+    },
+    TitleViewText: {
+        fontSize: 13,
+        fontWeight: "600",
+        color: "black"
+    },
+    OptionView: {
+        marginVertical: 20,
+        marginTop: 50,
+        marginHorizontal: 20
+    },
+    OptionViewText: {
+        fontSize: 21,
+        color: "black",
+        fontWeight: "700"
+    }
+});
 export default Home 
