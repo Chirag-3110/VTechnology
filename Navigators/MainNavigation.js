e129cd66c8cd6aef7a7b8569e2ad5ab538404e92
aayutha <thatheraaayushi@gmail.com>
2022-11-15 15:05:42 +0530

    added bottom bar

--- Navigators/MainNavigation.js
+++ Navigators/MainNavigation.js
@@ -1,33 +1,92 @@
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import {
+  Alert,
+  Animated,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Home from "../Screen/HomeScreen/Home";
 import Service from "../Screen/HomeScreen/Service";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-const Stack = createBottomTabNavigator();
+import { CurvedBottomBar } from 'react-native-curved-bottom-bar';
+import { NavigationContainer } from '@react-navigation/native';
 
-const MainNavigation = () => {
+const MainNavigator = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      {/* <NavigationContainer> */}
+        <CurvedBottomBar.Navigator
+          style={styles.bottomBar}
+          strokeWidth={1}
+          strokeColor="#FF00E1"
+          height={55}
+          circleWidth={55}
+          bgColor="white"
+          initialRouteName="Home"
+          // borderTopLeftRight
+          renderCircle={({ selectedTab, navigate }) => (
+            <Animated.View style={styles.btnCircle}>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                }}
+                onPress={() => Alert.alert('Click Action')}>
+              </TouchableOpacity>
+            </Animated.View>
+          )}
+          >
+          <CurvedBottomBar.Screen
+            name="Home"
+            position="LEFT"
+            component={Home}
+          />
+          <CurvedBottomBar.Screen
+            name="Service"
+            component={Service}
+            position="RIGHT"
+          />
+        </CurvedBottomBar.Navigator>
+      {/* </NavigationContainer> */}
+    </View>
+  );
+};
 
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown:false,
-                tabBarActiveTintColor: 'white',
-                tabBarShowLabel: false,
-                tabBarStyle: {
-                    // position: 'absolute',
-                    backgroundColor: 'pink',
-                    elevation: 15,
-                    height:60,
-                    // borderTopLeftRadius:100
-                    // bottom: 10,
-                    // left: 20,
-                    // right: 20,
-                    // height: 70,
-                },
-            }}
-        >
-            <Stack.Screen name="home" component={Home} />
-            <Stack.Screen name="Service" component={Service} />
-        </Stack.Navigator>
-    )
-}
-export default MainNavigation;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  button: {
+    marginVertical: 5,
+  },
+  bottomBar: {},
+  btnCircle: {
+    width: 60,
+    height: 60,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'cyan',
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 0.5,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 1,
+    bottom: 30,
+  },
+  imgCircle: {
+    width: 30,
+    height: 30,
+    tintColor: 'gray',
+  },
+  img: {
+    width: 30,
+    height: 30,
+  },
+});
+export default MainNavigator;
