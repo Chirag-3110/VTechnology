76a2a61a2efb7a81fba342c2c2c573b1aadda8d1
Chirag <takchirag828@gmail.com>
2022-11-25 15:25:02 +0530

    test screen

--- Screen/Questions/AllQuestions.js
+++ Screen/Questions/AllQuestions.js
@@ -1,164 +1,137 @@
 import React,{useEffect, useState} from 'react';
-import {View,Text,StyleSheet,ScrollView,Dimensions,TextInput,TouchableOpacity,ActivityIndicator} from 'react-native';
+import {View,Text,StyleSheet,ScrollView,Dimensions,Image,TouchableOpacity,ActivityIndicator} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import CustomButton from '../../components/AnimatedButton';
-import CustomDropDown from '../../components/CustomDropDown';
-import styles from './QuizStyles';
-const QuestionsCards=({route})=>{
-    const [loading, setLoading] = useState(false);
-    const {noOfQues}=route.params;
-//ques arrays
-    const [quizQues,setQuizQues]=useState([]);
-    const [optionsArray,setOptionArray]=useState(["","","",""]);
-    useEffect(()=>{
-        const questionsArray=[];
-        for (let i = 0; i <noOfQues; i++) {
-            questionsArray.push({
-                correctAnswerIndex:-1,
-                question:"",
-                options:[
-                    {_id:1,answer:"",option:"A"},
-                    {_id:2,answer:"",option:"B"},
-                    {_id:3,answer:"",option:"C"},
-                    {_id:4,answer:"",option:"D"},
-                ],
-                correctOptionDropDowns:[
-                    {label: 'A', value: 'A'},
-                    {label: 'B', value: 'B'},
-                    {label: 'C', value: 'C'},
-                    {label: 'D', value: 'D'},
-                ]
-            })
-        }
-        setQuizQues(questionsArray);
-    },[])
 
-    const handleQuesChange=(ques,selectedIndex)=>{
-        setQuizQues((question) =>
-            question.map((val,index) => {
-                if (index ===selectedIndex) {
-                    val.question = ques
-                }
-                return val;
-            })
-        );
-    }
-    const handleOptions=(options_val,quesindex,optIndex)=>{
-            setQuizQues((question) =>
-                question.map((val,index) => {
-                    if (index ===quesindex) {
-                        for (let i = 0; i < val.options.length; i++) {
-                            if(i===optIndex){
-                                val.options[i].answer=options_val;
-                                val.correctOptionDropDowns[i].label=options_val;
-                            }
-                        }
-                    }
-                    return val;
-                })
-            );
-    }
-    const handleCorrectOption=(item,quesindex)=>{
-        setQuizQues((question) =>
-            question.map((val,index) => {
-                if (index === quesindex) {
-                    for (let i = 0; i < val.options.length; i++) {
-                        if(item.value===val.options[i].option){
-                            val.correctAnswerIndex=i;
-                        }
-                    }
-                }
-                return val;
-            })
-        );
-    }
-    const uploadQuizData=()=>{
-         if(loading){
-            console.log("Please Wait for Adding New Questions");
-            return ;
-        }
-        //form validations
-        try {
-            quizQues.forEach(item => {
-                if(item.question==="")
-                    throw "Please Enter All The Questions";
-                if(item.correctAnswerIndex===-1)
-                    throw "Please select correct options for question";
-                item.options.forEach((optVal)=>{
-                    if(optVal==="")
-                        throw "Please Enter Options";
-                })
-            });
-            let updatedQuizFormat=changeQuizArrayFormat();
-            console.log(updatedQuizFormat);
-        } catch (error) {
-            alert(error)
-        }
-    }
-    const changeQuizArrayFormat=()=>{
-        let updatedQuizQuestionArray=[];
-        quizQues.forEach((item)=>{
-            updatedQuizQuestionArray.push({
-                correctAnswerIndex:item.correctAnswerIndex,
-                options:item.options,
-                question:item.question
-            });
-        })
-        return updatedQuizQuestionArray;
-    }
+const windoWidth=Dimensions.get('window').width;
+const QuestionsCards=()=>{
+   
+    const [ques,setQues]=useState([
+        {
+            id:1,
+            quesion:"What is C ?",
+            options:["language","Function","os","none"],
+            corretOptionsIndex:0,
+        },
+        {
+            id:2,
+            quesion:"What is C ?",
+            options:["language","Function","os","none"],
+            corretOptionsIndex:0,
+        },
+        {
+            id:3,
+            quesion:"What is C ?",
+            options:["language","Function","os","none"],
+            corretOptionsIndex:0,
+        },
+        {
+            id:4,
+            quesion:"What is C ?",
+            options:["language","Function","os","none"],
+            corretOptionsIndex:0,
+        },
+        {
+            id:5,
+            quesion:"What is C ?",
+            options:["language","Function","os","none"],
+            corretOptionsIndex:0,
+        },
+    ])
     return(
-        <ScrollView style={styles.Quescontainer} showsVerticalScrollIndicator={false}>
-            <Text style={{padding:10,fontWeight:"bold",fontSize:20,color:"black",marginTop:30}}>Add Questions</Text>
-            {
-                quizQues.map((item,index)=>(
-                    <LinearGradient 
-                        key={index}
-                        colors={['rgba(245,230,252,0.80)', 'rgba(246,229,254,0.80)','rgba(249,243,252,0.80)']} 
-                        style={styles.infoContainer}>
-                        <View style={{paddingHorizontal:20,alignSelf:"center",marginTop:10}}>
-                            <Text style={{fontWeight:"bold",color:"black"}}>Question {index+1}</Text>
-                            <TextInput
-                                placeholder='Enter question'
-                                placeholderTextColor={"black"}
-                                style={styles.customInput}
-                                onChangeText={(ques)=>handleQuesChange(ques,index)}
-                            />
-                            <View style={{paddingVertical:10}}>
-                                <Text style={{fontWeight:"bold",color:"black"}}>Options</Text>
-                                {
-                                    optionsArray.map((opt,optIndex)=>(
-                                        <TextInput
-                                            placeholder={`Enter option ${optIndex+1}`}  
-                                            placeholderTextColor={"black"}
-                                            style={styles.customInput}
-                                            onChangeText={(options_val)=>handleOptions(options_val,index,optIndex)}
-                                        />
-                                    ))
-                                }
-                            </View>
-                            {/* <Text style={{fontWeight:"bold",color:"black"}}>Correct answer</Text>
-                            <CustomDropDown
-                                data={item.correctOptionDropDowns}
-                                label="Select one"
-                                labelStyle={styles.dropDownLabelStyle}
-                                dropDownStyle={styles.dropDownContainerStyle}
-                                itemStyle={styles.dropDownItemStyle}
-                                onSelectValue={(item)=>handleCorrectOption(item,index)}
-                            /> */}
-                        </View>
-                    </LinearGradient>
-                ))
-            }
-            <View style={{marginBottom:30}}>
-                {/* {
-                    loading?
-                    <ActivityIndicator size={30} color="blue" style={{margin:50}}/>:
-                    <CustomButton
-                        title="Post Questions"
-                    />
-                } */}
+       <ScrollView style={styles.container}>
+            <View style={{ width: windoWidth ,padding:10,flexDirection:"row",justifyContent:"space-between",alignItems: 'center',alignSelf:"center"}}>
+                <Image source={{ uri: "https://cdn3d.iconscout.com/3d/premium/thumb/male-character-sitting-on-chair-and-reading-a-book-4634471-3855676.png" }} style={styles.MainImg} />
+                <Text style={{color:"white",fontWeight:"bold",fontSize:60,width:170,}}>Quiz Time</Text>
             </View>
-        </ScrollView>
+            <View style={styles.Quiztitle}>
+                <Text style={styles.quiCardTitle}>Hello there, Quiz Time</Text>
+                <Text style={styles.quizCardSubText}>Digital Maketing Quiz</Text>
+                <Text style={styles.quizCardMiniText}>
+                    Boost your performance by giving Quizes
+                </Text>
+            </View>
+            <View style={styles.Quiztitle}>
+                <View style={[styles.quiCardTitle,{flexDirection: 'row',alignItems:"center",justifyContent: 'space-evenly',}]}>
+                    <Text style={{fontWeight:"bold",fontSize:20,color:"rgba(115,105,248,0.85)"}}>Questions</Text>
+                    <Text style={{color:"white",fontWeight:"bold",fontSize:20,backgroundColor:"rgba(115,105,248,0.85)",padding:5,borderRadius:5,paddingHorizontal:10}}>5</Text>
+                </View>
+                {
+                    ques.map((items,index)=>(
+                        <View style={styles.quesCard}>
+                            <Text style={{fontWeight:"bold",color:"black",fontSize:22,width:'95%',paddingVertical:10}}>
+                                Q{index+1}. {items.quesion}
+                            </Text>
+                            {
+                                items.options.map((value,index)=>(
+                                    <Text style={{fontWeight:"bold",color:"#2e2e2f",fontSize:20,width:'95%',padding:2}}>
+                                        {index+1}. {value}
+                                    </Text>
+                                ))
+                            }
+                        </View>
+                    ))
+                }
+            </View>
+       </ScrollView>
     )
 }
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"rgba(115,105,248,0.85)"
+    },
+    MainImg: {
+        width: 150,
+        height: 200,
+    },
+    Quiztitle:{
+        width:windoWidth-30,
+        padding:15,
+        borderRadius:10,
+        backgroundColor:"white",
+        alignSelf:"center",
+        elevation:50,
+        shadowColor:"#1000ff",
+        marginBottom:20,
+        shadowOffset:{
+            width:0,
+            height:-20
+        }
+    },
+    quiCardTitle:{
+        color:"rgba(115,105,248,0.85)",
+        fontWeight:"bold",
+        fontSize:16,
+        width:'70%',
+        backgroundColor:"rgba(198,194,250,0.40)",
+        textAlign:"center",
+        padding:10,
+        borderRadius:10,
+        marginVertical:5,
+    },
+    quizCardSubText:{
+        color:"#2e2e2f",
+        fontWeight:"bold",
+        fontSize:20,
+        paddingHorizontal:10,
+        paddingVertical:5,
+    },
+    quizCardMiniText:{
+        color:"#7f7f7f",
+        fontWeight:"800",
+        fontSize:15,
+        paddingHorizontal:10,
+        paddingVertical:5,
+        width:'90%'
+    },
+    quesCard:{
+        backgroundColor:"red",
+        padding:10,
+        backgroundColor:"rgba(198,194,250,0.40)",
+        borderRadius:10,
+        alignItems: 'center',
+        marginVertical:10,
+        paddingVertical:20
+    }
+})
 export default QuestionsCards;
