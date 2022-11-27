8e9f66cb9db6b897863f6a6955b445c227bf2d7c
abhijan2402 <abhishek.jangid643@gmail.com>
2022-11-27 23:00:53 +0530

    Adding and Updating Again and agian

--- Screen/HomeScreen/CourseDetail.js
+++ Screen/HomeScreen/CourseDetail.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, Animated, Image, StyleSheet, Modal, Dimensions, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, Animated, Image, StyleSheet, Modal, Dimensions, TouchableOpacity, ImageBackground } from 'react-native'
 import { LineChart, } from "react-native-chart-kit";
 const windoWidth = Dimensions.get('window').width;
 const windoHeight = Dimensions.get('window').height;
+const image = { uri: "https://cdn3d.iconscout.com/3d/premium/thumb/man-with-ok-gesture-showing-business-charts-in-laptop-screen-4929412-4122896.png" };
 function CourseDetail() {
     const [modalVisible, setModalVisible] = useState(false);
     useEffect(() => {
@@ -11,9 +12,12 @@ function CourseDetail() {
 
     return (
         <View style={styles.MainView}>
-            <View style={styles.MainTopview}>
-                <Image source={{ uri: "https://cdn3d.iconscout.com/3d/premium/thumb/man-with-ok-gesture-showing-business-charts-in-laptop-screen-4929412-4122896.png" }} style={[styles.ProImg]} />
-            </View>
+            <ImageBackground source={image} resizeMode="cover" style={styles.MainTopview}>
+                <View style={{ marginHorizontal: 20, marginVertical: 20 }}>
+                    <Image source={{ uri: "https://cdn-icons-png.flaticon.com/128/4518/4518010.png" }} style={{ width: 35, height: 35 }} />
+
+                </View>
+            </ImageBackground>
             <View style={styles.centeredView}>
                 <Modal
                     animationType="slide"
@@ -26,27 +30,30 @@ function CourseDetail() {
                 >
                     <View style={styles.centeredView}>
                         <View style={styles.modalView}>
-                            <View style={styles.ModelTopView}>
-                                <View style={styles.ContentView}>
-
-                                    <Text style={styles.ModelTopViewText}>Get your position in digital marketing</Text>
+                            <Text style={styles.CourseName}>Digital Marketing</Text>
+                            <Text style={styles.CourseAdmin}>By Maddi Son (Exp 15 year)</Text>
+                            <View style={{ display: "flex", flexDirection: "row" }}>
+                                <View style={styles.LessonDetail}>
+                                    <Image source={{ uri: "https://cdn-icons-png.flaticon.com/128/9045/9045135.png" }} style={{ width: 23, height: 23 }} />
+                                    <Text style={styles.Question}>5 Question</Text>
                                 </View>
-                                {/* <TouchableOpacity style={[styles.ContentView, { justifyContent: "flex-end", alignItems: "flex-end" }]} onPress={() => setModalVisible(!modalVisible)}>
-                                    <Image source={{ uri: "https://cdn-icons-png.flaticon.com/128/656/656857.png" }} style={[styles.ProImg, { width: 20, height: 20, marginRight: 30 }]} />
-
-                                </TouchableOpacity> */}
+                                <View style={styles.LessonDetail}>
+                                    <Image source={{ uri: "https://cdn-icons-png.flaticon.com/128/3073/3073440.png" }} style={{ width: 23, height: 23 }} />
+                                    <Text style={styles.Question}>5 min</Text>
+                                </View>
                             </View>
-                            <View style={styles.details}>
-                                <Text style={styles.detailsText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus aspeorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</Text>
+                            <View style={{ marginTop: 10 }}>
+                                <Text style={{ fontSize: 17, fontWeight: "800", color: 'black' }}>Description</Text>
+                                <Text style={{}}>Lorem ipsum” dummy text isith real data. Often, developers use third-party services to generate “Lorem ipsum” text, but now you can do that right in your editor. Just expand lorem or lipsum abbreviations to get the following snippet:</Text>
                             </View>
-                            <TouchableOpacity style={[styles.Modelviewbtn]}>
-                                <Text style={styles.ModelviewTextbtn}>Enroll Now</Text>
+                            <TouchableOpacity style={styles.ActivityStartBtn}>
+                                <Text style={styles.ActivityStartBtnText}>Start Activity</Text>
                             </TouchableOpacity>
                         </View>
                     </View>
                 </Modal>
-            </View>
-        </View>
+            </View >
+        </View >
     )
 }
 const styles = StyleSheet.create({
@@ -55,16 +62,13 @@ const styles = StyleSheet.create({
         height: windoHeight,
     },
     MainTopview: {
-        backgroundColor: "rgba(115,105,248,0.85)"
-    },
-    ProImg: {
+        backgroundColor: "rgba(115,105,248,0.85)",
         width: windoWidth,
         height: windoHeight / 1.8
     },
     centeredView: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
         position: "absolute",
         bottom: 0,
     },
@@ -75,7 +79,6 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         padding: 35,
         paddingTop: 25,
-        alignItems: "center",
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -88,87 +91,55 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 35,
         borderTopRightRadius: 35
     },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2
-    },
-    buttonOpen: {
-        backgroundColor: "#F194FF",
-    },
-    buttonClose: {
-        backgroundColor: "#2196F3",
-    },
-    textStyle: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: "center"
-    },
-    ModelView: {
-        // position: 'absolute',
-        zIndex: 1,
-        backgroundColor: "white",
-        width: windoWidth,
-        marginTop: 20,
-        height: windoHeight / 2,
-        bottom: 10,
-        borderWidth: 1,
-        borderTopLeftRadius: 40,
-        borderTopRightRadius: 40
-    },
-    ModelTopView: {
-        // borderWidth: 1,
-        marginVertical: 15,
-        // marginHorizontal: 30,
-        width: windoWidth / 1.2,
-        display: "flex",
-        flexDirection: "row",
-        // justifyContent: "center",
-        // alignItems: "center"
-    },
-    ModelTopViewText: {
-        fontSize: 20,
-        fontWeight: "700",
+    CourseName: {
+        fontSize: 27,
+        fontWeight: "800",
         color: "black"
     },
-    ContentView: {
-        // borderWidth: 1,
-        width: windoWidth / 1.2
-    },
-    ModelTopTextPrice: {
-        marginVertical: 10,
-        fontSize: 20,
+    CourseAdmin: {
+        fontSize: 15,
         color: "black",
-        fontWeight: "800",
-        borderWidth: 1,
+        marginVertical: 5,
+        marginBottom: 15
     },
-    PriceView: {
-        marginHorizontal: 25
-    },
-    Modelviewbtn: {
-        width: windoWidth / 1.3,
-        borderWidth: 1,
-        backgroundColor: "black",
-        marginHorizontal: 10,
-        marginVertical: 20,
-        borderRadius: 20,
-        paddingVertical: 10,
+    LessonDetail: {
+        display: "flex",
+        flexDirection: 'row',
         alignItems: "center",
-        opacity: 0.9
+        borderWidth: 1,
+        borderColor: "lightgrey",
+        width: windoWidth / 2.8,
+        padding: 5,
+        borderRadius: 7,
+        marginRight: 10
     },
-    ModelviewTextbtn: {
-        color: "white",
-        fontSize: 20,
-        fontWeight: "700"
+    Question: {
+        fontSize: 17,
+        color: "black",
+        marginHorizontal: 5
     },
-    details: {
-        marginHorizontal: 0,
-        fontWeight: "700",
-        width: windoWidth / 1.2
+    ActivityStartBtn: {
+        borderWidth: 1,
+        borderColor: "rgba(115,105,248,0.85)",
+        alignItems: "center",
+        paddingVertical: 7,
+        borderRadius: 10,
+        marginTop: 16,
+        backgroundColor: "rgba(115,105,248,0.85)",
+        marginHorizontal: 20,
+        shadowColor: 'rgba(115,105,248,0.85)',
+        shadowOffset: {
+            width: 6,
+            height: -5,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 400,
+        elevation: 13,
     },
+    ActivityStartBtnText: {
+        fontSize: 19,
+        color: "white"
+    }
+
 })
 export default CourseDetail
