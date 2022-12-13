ac56acc7fe67c3fb934ce65087633038d6d00768
Chirag <takchirag828@gmail.com>
2022-12-13 22:21:18 +0530

    validation remove

--- Validate/EmailValidation.js
+++ Validate/EmailValidation.js
@@ -1,6 +1,5 @@
 const EmailValidate=(email)=>{
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if(!email.match(validRegex) || !email.includes("gmail.com")){
+    if(!email.includes("gmail.com")){
         return  false
     }
     return true
